77a3954c8c807487497f1a6a26464a677cfd7dd3
coolone <coolone.official@gmail.com>
2019-04-16 10:36:33 +0300

    Super calc but with some login bugs

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -1,10 +1,10 @@
 <template>
     <v-container>
         <v-card>
-            <v-input></v-input>
             <v-layout row>
-                <v-text-field v-model="input" outline></v-text-field>
-                <h1>{{  }}</h1>
+                <v-text-field v-model="inputText" :suffix="solution.length > 0 && solution !== 'NaN' ? '=' + solution : ''"
+                              solo></v-text-field>
+                <h1>{{ }}</h1>
             </v-layout>
         </v-card>
     </v-container>
@@ -13,29 +13,74 @@
 <script>
     export default {
         data: () => ({
-            input: "",
-            solution: "",
-            watch: {
-                input() {
-                    let list = this.input.split(/[0-9]*[+|\-\\*/]?/);
-
-                    let pair;
-                    for(let str in list) {
-                        pair = [str.slice(0, str.length - 2), str[str.length - 1]];
-
-
-                    }
-                }
-            },
-            methods: {
-                compute(str) {
-
-                }
-            },
+            inputText: "",
+            solution: "0",
         }),
+        watch: {
+            inputText(inputText) {
+                console.log(inputText);
+                this.solution = this.compute(inputText);
+            }
+        },
+        methods: {
+            compute(str) {
+                function isAction(str) {
+                    return str === "+" || str === "-" || str === "/" || str === "*";
+                }
+
+                let list = str.split(/([+\-/*?]\w*)/).filter(value => value.length > 0);
+
+                console.log(list);
+
+                let action, number, finalResult = "0";
+                for (let str of list) {
+                    if (isAction(str[0])) {
+                        action = str[0];
+                        number = str.substr(1);
+                    } else {
+                        action = null;
+                        number = str;
+                    }
+                    number = parseInt(number);
+
+                    console.log('action:', action);
+                    console.log('num:', number);
+
+                    let result = parseInt(finalResult);
+                    console.log("result before:", finalResult," to ", result);
+
+                    switch (action) {
+                        case '-':
+                            result -= number;
+                            break;
+                        case '+':
+                            result += number;
+                            break;
+                        case '/':
+                            result /= number;
+                            break;
+                        case '*':
+                            result *= number;
+                            break;
+                        default:
+                            result += number;
+                            break;
+                    }
+                    finalResult = result.toString();
+                    console.log('result after: ', result.toString(), ' to ', finalResult);
+                }
+
+
+                console.log('---------', finalResult);
+
+                return finalResult;
+            }
+        },
     }
 </script>
 
 <style>
+    html {
 
+    }
 </style>
